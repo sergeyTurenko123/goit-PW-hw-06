7af0eb8ee0979a693c7b982574f5a81f1cd9c10e
sergeyTurenko123 <sergej_petrovich_8484@ukr.net>
2024-04-27 01:17:31 +0300

    dz_6

---
+++ EX-08/venv/Scripts/salary.sql
@@ -0,0 +1,44 @@
+-- Table: groups
+DROP TABLE IF EXISTS groups;
+CREATE TABLE groups (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  group_number VARCHAR(255) UNIQUE NOT NULL);
+
+-- Table: students
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student VARCHAR(255) UNIQUE NOT NULL,
+    student_id INTEGER,
+    FOREIGN KEY (student_id) REFERENCES groups (id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+);
+
+-- Table: teachers
+DROP TABLE IF EXISTS teachers;
+CREATE TABLE teachers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    teacher VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Table: subjects
+DROP TABLE IF EXISTS subjects;
+CREATE TABLE subjects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    subject VARCHAR(255) UNIQUE NOT NULL,
+    subject_id INTEGER,
+    FOREIGN KEY (subject_id) REFERENCES teachers (id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+);
+
+-- Table: evaluations
+DROP TABLE IF EXISTS evaluations;
+CREATE TABLE evaluations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student VARCHAR(255) UNIQUE NOT NULL,
+    subject VARCHAR(255) UNIQUE NOT NULL,
+    evaluation VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
